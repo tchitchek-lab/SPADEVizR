8f1ff3aecbf6d8c86fc2be2b77561328ab468d4e
nicolas-tchitchek <nicolas.tchitchek@cea.fr>
2016-11-14 10:59:10 +0100

    add of the annotateClusters functionality

--- R/misc.R
+++ R/misc.R
@@ -83,3 +83,65 @@ mergeClusters <- function(Results, clusters, name){
 	
 	invisible(newResults)
 }
+
+
+#' @title Annotating cell clusters
+#' 
+#' @description 
+#' This function is used to annotate cell clusters based on their marker expression categories.
+#' The annotations of cell clusters must be specifyed using a character dataframe indicating for each marker of each population the matching categorial values.
+#' 
+#' @details
+#' The annotation data.frame must have the annotations in rows and the markers in colomuns.
+#' Matching values must be specified as a character list of acceptable values. 
+#' 
+#' @param Results a Results object
+#' @param annotations a character dataframe specifying the annotations 
+#' @param num a numeric value specifying the number of markers expression categories to be used
+#'
+#' @return a Results object
+#'
+#' @export
+annotateClusters <- function(Results, annotations, num=5){
+	
+	newResults <- Results
+	
+	if(!is.data.frame(annotations)){
+		stop(paste("error in annotateClusters: annotations is not a dataframe"))
+	}
+	
+	if(any(!colnames(annotations) %in% newResults@marker.names)){
+		unknown <- colnames(annotations)[!colnames(annotations) %in% newResults@marker.names]
+		stop(paste("error in annotateClusters: markers in annotations dataframe (",unknown,") are not in markers of the Results object"))
+	}
+	
+	hm <- heatmapViewer(newResults,num=num,dendrograms="none",show.on_device=FALSE)$pheno.table
+	
+	for(cluster in colnames(hm)){
+		pheno_hm <- hm[,cluster]
+		
+		for(annotation in rownames(annotations)){
+			pheno_annot <- annotations[annotation,]
+		
+			chk = TRUE
+			for(marker in colnames(pheno_annot)[!is.na(pheno_annot)]){
+				toeval = paste0(pheno_hm[marker], " %in% ", pheno_annot[marker])
+				if(!eval(parse(text=toeval))){
+					chk = FALSE
+				}
+			}
+			
+			if(chk==TRUE){
+				newname <- paste0(cluster,":",annotation) 
+				
+				if(!is.null(newname)){
+					newResults@cluster.names[newResults@cluster.names==cluster] <- newname
+					newResults@cluster.phenotypes$cluster[newResults@cluster.phenotypes$cluster==cluster] <- newname
+				}
+			}
+		}
+		
+	}
+	
+	invisible(newResults)
+}
